1453933296c32de10182a9e4a7b1529e1732c130
Shaun Hoyes <jshoyes@gmail.com>
2017-03-02 10:38:49 -0700

    elminate numberWithCommas function

--- btc-price-data.js
+++ btc-price-data.js
@@ -2,12 +2,7 @@
 var request = require('request');
 var bitfinexUrl = "https://api.bitfinex.com/v1";
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
-};
-
 request.get(bitfinexUrl + "/pubticker/BTCUSD", function(error, response, body) {
     var data = JSON.parse(body);
     console.log(data);
-    // console.log("$" + numberWithCommas(data.last_price) + " USD");
 });
