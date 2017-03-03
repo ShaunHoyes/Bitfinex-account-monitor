f8910f15a12cff0ca53afc25633cce863868dd99
Shaun Hoyes <jshoyes@gmail.com>
2017-03-02 17:42:30 -0700

    platform lending stats

---
+++ lends.js
@@ -0,0 +1,19 @@
+const
+  request = require('request')
+  url = `https://api.bitfinex.com/v1`
+  payload = {
+  "timestamp": false,
+  "limit_lends": 1
+},
+  options = {
+    url: url + `/lends/USD`,
+    qs: payload
+  };
+
+request.get(options, function(error, response, body) {
+    var data = JSON.parse(body);
+    console.log(`Bitfinex lending stats:\n`)
+    console.log(`Interest rate: ${data[0]['rate']}%`);
+    console.log(`Lent: ${data[0]['amount_lent']} USD`);
+    console.log(`Used: ${data[0]['amount_used']} USD`);
+});
